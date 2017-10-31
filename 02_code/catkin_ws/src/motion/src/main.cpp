3d90e4376f4b9c5fd064f21877f7d8a192983ced
Unknown <mbertram@uni-bremen.de>
2017-10-31 12:11:33 +0100

    first movements

--- 02_code/catkin_ws/src/motion/src/main.cpp
+++ 02_code/catkin_ws/src/motion/src/main.cpp
@@ -1,5 +1,7 @@
 #include <ros/node_handle.h>
 #include <ros/ros.h>
+#include <moveit/move_group_interface/move_group.h>
+#include <iostream>
 
 class Main {
 private:
@@ -7,16 +9,28 @@ private:
 
 public:
     Main(const ros::NodeHandle &nh) :
-            node_handle(nh)
-            {
+            node_handle(nh) {
 
     }
 };
 
-int main(int argc, char **argv) {
-	ROS_INFO("hallo");
-        ros::init(argc, argv, "main_motion");
-        ros::NodeHandle nh;
-        Main main(nh);
-        ros::spin();
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "movement_example1");
+    ros::NodeHandle node_handle;
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    // Part of the robot to move
+    moveit::planning_interface::MoveGroup group("right_arm");
+
+    // specify that our target will be a random one
+    group.setRandomTarget();
+
+    // plan the motion and then move the group to the sampled target
+    group.move();
+
+    return 0;
 }
+
+
