a14c9c67a056ae175fbc142379a002c07b9ef942
Unknown <mbertram@uni-bremen.de>
2017-11-03 15:35:06 +0100

    listen to command 1

--- 02_code/catkin_ws/src/motion/src/main.cpp
+++ 02_code/catkin_ws/src/motion/src/main.cpp
@@ -1,20 +1,22 @@
 #include <ros/node_handle.h>
 #include <ros/ros.h>
 #include <moveit/move_group_interface/move_group.h>
-#include <iostream>
 #include <actionlib/server/simple_action_server.h>
 #include <motion/MovingCommandAction.h>
-#include <string>
 #include <std_msgs/String.h>
+
 
 class Main {
 private:
     ros::NodeHandle node_handle;
+    moveit::planning_interface::MoveGroup group;
+    geometry_msgs::Pose target_pose1;
     actionlib::SimpleActionServer<motion::MovingCommandAction> action_server;
 
 public:
     Main(const ros::NodeHandle &nh) :
             node_handle(nh),
+            group("right_arm"),
             action_server(node_handle, "moving", boost::bind(&Main::executeCommand, this, _1), false)
             {
             	action_server.start();
@@ -24,7 +26,25 @@ public:
 	void executeCommand(const motion::MovingCommandGoalConstPtr &goal){
 	    switch(goal->command) {
             case 1:
-                ROS_INFO("Moving to standard pose");
+//                ROS_INFO("Moving to standard pose");
+                //moveit::planning_interface::MoveGroup group("right_arm");
+                //moveit::planning_interface::MoveGroup leftgroup("left_arm");
+
+                target_pose1.orientation.w = 1.0;
+                target_pose1.position.x = 0.28;
+                target_pose1.position.y = -0.7;
+                target_pose1.position.z = 1;
+                group.setPoseTarget(target_pose1);
+
+                /*geometry_msgs::Pose target_pose2;
+                target_pose2.orientation.w = 1.0;
+                target_pose2.position.x = 0.28;
+                target_pose2.position.y = 0.7;
+                target_pose2.position.z = 1;
+                leftgroup.setPoseTarget(target_pose2);*/
+
+                group.move();
+                //leftgroup.move();
                 break;
             case 2:
                 ROS_INFO("Moving right arm");
@@ -45,9 +65,7 @@ int main(int argc, char **argv)
     ros::NodeHandle node_handle;
 
     Main main(node_handle);
-
     ros::spin();
-    
     /*ros::AsyncSpinner spinner(1);
     spinner.start();
 
