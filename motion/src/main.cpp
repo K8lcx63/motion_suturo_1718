60dedd3f6326db4e2592ac509a8f17df2797f18b
Unknown <rhaak@uni-bremen.de>
2018-01-16 10:58:22 +0100

    errorstatus für hauke

--- motion/src/main.cpp
+++ motion/src/main.cpp
@@ -125,173 +125,174 @@ public:
         switch(error_code.val){
             case moveit_msgs::MoveItErrorCodes::SUCCESS:
                 result.successful = true;
+                result.status = motion_msgs::MovingCommandResult::SUCCESS;
                 action_server.setSucceeded(result);
                 ROS_INFO("\x1B[32mX: Moved successfully to goal.");
                 break;
             case moveit_msgs::MoveItErrorCodes::FAILURE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "FAILURE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("FAILURE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::PLANNING_FAILED:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::OUT_OF_RANGE;
                 action_server.setAborted(result, "PLANNING FAILED");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("PLANNING FAILED.");
                 break;
             case moveit_msgs::MoveItErrorCodes::INVALID_MOTION_PLAN:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "INVALID MOTION PLAN");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("INVALID MOTION PLAN.");
                 break;
             case moveit_msgs::MoveItErrorCodes::MOTION_PLAN_INVALIDATED_BY_ENVIRONMENT_CHANGE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "MOTION PLAN INVALIDATED BY ENVIRONMENT CHANGE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("MOTION PLAN INVALIDATED BY ENVIRONMENT CHANGE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::CONTROL_FAILED:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "CONTROL FAILED");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("CONTROL FAILED.");
                 break;
             case moveit_msgs::MoveItErrorCodes::UNABLE_TO_AQUIRE_SENSOR_DATA:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "UNABLE TO AQUIRE SENSOR DATA");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("UNABLE TO AQUIRE SENSOR DATA.");
                 break;
             case moveit_msgs::MoveItErrorCodes::TIMED_OUT:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "TIMED OUT");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("TIMED OUT.");
                 break;
             case moveit_msgs::MoveItErrorCodes::PREEMPTED:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "PREEMPTED");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("PREEMPTED.");
                 break;
             case moveit_msgs::MoveItErrorCodes::START_STATE_IN_COLLISION:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::COLLISION;
                 action_server.setAborted(result, "START STATE IN COLLISION");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("START STATE IN COLLISION.");
                 break;
             case moveit_msgs::MoveItErrorCodes::START_STATE_VIOLATES_PATH_CONSTRAINTS:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "START STATE VIOLATES PATH CONSTRAINTS");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("START STATE VIOLATES PATH CONSTRAINTS.");
                 break;
             case moveit_msgs::MoveItErrorCodes::GOAL_IN_COLLISION:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::COLLISION;
                 action_server.setAborted(result, "GOAL IN COLLISION");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("GOAL IN COLLISION.");
                 break;
             case moveit_msgs::MoveItErrorCodes::GOAL_VIOLATES_PATH_CONSTRAINTS:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "GOAL VIOLATES PATH CONSTRAINTS");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("GOAL VIOLATES PATH CONSTRAINTS.");
                 break;
             case moveit_msgs::MoveItErrorCodes::GOAL_CONSTRAINTS_VIOLATED:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "GOAL CONSTRAINTS VIOLATED");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("GOAL CONSTRAINTS VIOLATED.");
                 break;
             case moveit_msgs::MoveItErrorCodes::INVALID_GROUP_NAME:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "INVALID GROUP NAME");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("INVALID GROUP NAME.");
                 break;
             case moveit_msgs::MoveItErrorCodes::INVALID_GOAL_CONSTRAINTS:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "INVALID GOAL CONSTRAINTS");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("INVALID GOAL CONSTRAINTS.");
                 break;
             case moveit_msgs::MoveItErrorCodes::INVALID_ROBOT_STATE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "INVALID ROBOT STATE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("INVALID ROBOT STATE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::INVALID_LINK_NAME:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "INVALID LINK NAME");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("INVALID LINK NAME.");
                 break;
             case moveit_msgs::MoveItErrorCodes::INVALID_OBJECT_NAME:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "INVALID OBJECT NAME");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("INVALID OBJECT NAME.");
                 break;
             case moveit_msgs::MoveItErrorCodes::FRAME_TRANSFORM_FAILURE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "FRAME TRANSFORM FAILURE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("FRAME TRANSFORM FAILURE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::COLLISION_CHECKING_UNAVAILABLE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "COLLISION CHECKING UNAVAILABLE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("COLLISION CHECKING UNAVAILABLE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::ROBOT_STATE_STALE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "ROBOT STATE STALE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("ROBOT STATE STALE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::SENSOR_INFO_STALE:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "SENSOR INFO STALE");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("SENSOR INFO STALE.");
                 break;
             case moveit_msgs::MoveItErrorCodes::NO_IK_SOLUTION:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "NO IK SOLUTION");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("NO IK SOLUTION.");
                 break;
             default:
                 result.successful = false;
-
+                result.status = motion_msgs::MovingCommandResult::UNMANAGEBLE_ERROR;
                 action_server.setAborted(result, "UNKNOWN ERROR");
                 ROS_ERROR("Movement aborted. Errorcode: ");
                 ROS_ERROR("UNKNOWN ERROR.");
