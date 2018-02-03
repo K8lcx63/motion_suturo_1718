c50acd2018b6d61a28fedd903793d838d657e75f
Unknown <mbertram@uni-bremen.de>
2018-02-03 02:01:56 -0800

    add transform function

--- suturo_motion_main/src/movegroup/group_controller.cpp
+++ suturo_motion_main/src/movegroup/group_controller.cpp
@@ -1,21 +1,31 @@
 #include "group_controller.h"
 
+/**
+ * Transforms a given {@link geometry_msgs::PointStamped} to the matching PointStamped
+ * in the planningframe of the given {@link moveit::planning_interface::MoveGroup}.
+ *
+ * @param group the group to whichs planning frame the point should be transformed.
+ * @param point the point to transform.
+ * @return the transformed {@link geometry_msgs::PointStamped}.
+ */
+geometry_msgs::PointStamped
+GroupController::transformPointStamped(moveit::planning_interface::MoveGroup& group, const geometry_msgs::PointStamped& point) {
+    if (group.getPlanningFrame() == point.header.frame_id) {
+        ROS_INFO("TRANSFORMING POINT");
+        ROS_INFO("from: %s to: %s", point.header.frame_id.c_str(), group.getPlanningFrame().c_str());
+        geometry_msgs::PointStamped return_point;
+        listener.transformPoint(group.getPlanningFrame(), point, return_point);
+        return return_point;
+    }
+    ROS_INFO("POINT ALREADY IN RIGHT FRAME");
+    return point;
+}
+
 moveit_msgs::MoveItErrorCodes
-GroupController::moveGroupToCoordinates(moveit::planning_interface::MoveGroup &group, const geometry_msgs::PointStamped &goal_point) {
-    geometry_msgs::PointStamped point;
+GroupController::moveGroupToCoordinates(moveit::planning_interface::MoveGroup& group, const geometry_msgs::PointStamped& goal_point) {
+    geometry_msgs::PointStamped point = transformPointStamped(group, goal_point);
 
     //publishVisualizationMarker(goal_point, COLOR_SCHEMA_KNOWLEDGE);
-    geometry_msgs::PointStamped tempPoint;
-    tempPoint.header = goal_point.header;
-    tempPoint.point.x = goal_point.point.x;
-    tempPoint.point.y = goal_point.point.y;
-    tempPoint.point.z = goal_point.point.z;
-    ROS_INFO("Transforming Point from %s to %s", goal_point.header.frame_id.c_str(), group.getPlanningFrame().c_str());
-    listener.transformPoint(group.getPlanningFrame(), tempPoint, point);
-    ROS_INFO("----Transformed point----");
-    ROS_INFO("x %g", point.point.x);
-    ROS_INFO("y %g", point.point.y);
-    ROS_INFO("z %g", point.point.z);
 
     geometry_msgs::PoseStamped poseStamped;
     poseStamped.header.frame_id = point.header.frame_id;
--- suturo_motion_main/src/movegroup/group_controller.h
+++ suturo_motion_main/src/movegroup/group_controller.h
@@ -10,10 +10,28 @@ class GroupController {
 private:
     tf::TransformListener listener;
     moveit::planning_interface::MoveGroup::Plan execution_plan;
+
+    geometry_msgs::PointStamped
+    transformPointStamped(moveit::planning_interface::MoveGroup &group, const geometry_msgs::PointStamped &point);
+
 public:
+    /**
+     * Moves the given {@link moveit::planning_interface::MoveGroup}
+     * to the desired coordinates provided in {@link geometry_msgs::PointStamped}.
+     *
+     * @param group the group to move.
+     * @param goal_point the coordinates to mvoe the group to.
+     * @return {@link moveit_msgs::MoveItErrorCodes} with the result of the movement.
+     */
     moveit_msgs::MoveItErrorCodes
     moveGroupToCoordinates(moveit::planning_interface::MoveGroup &group, const geometry_msgs::PointStamped &goal_point);
 
+    /**
+     * Moves the given {@link moveit::planning_interface::MoveGroup} to the initial pose.
+     *
+     * @param group the group to move.
+     * @return {@link moveit_msgs::MoveItErrorCodes} with the result of the movement.
+     */
     moveit_msgs::MoveItErrorCodes moveArmsToInitial(moveit::planning_interface::MoveGroup &group);
 };
 
