08753cccc6fed2df5d39c1ad8aad1a642f248897
Unknown <rhaak@uni-bremen.de>
2018-02-13 09:29:24 +0100

    mit trajectory zum gerade heranfahren

--- motion/src/main.cpp
+++ motion/src/main.cpp
@@ -13,6 +13,7 @@
 #include <moveit/robot_state/robot_state.h>
 #include <moveit/robot_trajectory/robot_trajectory.h>
 #include <moveit/robot_model_loader/robot_model_loader.h>
+#include <math.h>
 
 const int COLOR_SCHEMA_MOTION = 0;
 const int COLOR_SCHEMA_KNOWLEDGE= 1;
@@ -426,12 +427,9 @@ public:
         geometry_msgs::PointStamped frontDirectionOfObject;
         listener.transformPoint("base_footprint", goal_point, frontDirectionOfObject);
 
-        //calculate position in front of object and half of the way to the robot
-        frontDirectionOfObject.point.x = frontDirectionOfObject.point.x / 2;
+        //calculate position in front of object and two-third of the way to the robot
+        frontDirectionOfObject.point.x = frontDirectionOfObject.point.x * 3/4;
         publishVisualizationMarker(frontDirectionOfObject, COLOR_SCHEMA_KNOWLEDGE);
-        ROS_INFO("X: %g", frontDirectionOfObject.point.x);
-        ROS_INFO("Y: %g", frontDirectionOfObject.point.y);
-        ROS_INFO("Z: %g", frontDirectionOfObject.point.z);
 
         //transform to PoseStamped and set orientation
         geometry_msgs::PoseStamped frontDirectionOfObjectPose;
@@ -467,105 +465,231 @@ public:
                 inFrontOfObjectEndEffectorPose.position.x -= GRIPPER_LENGTH_LEFT;
             }
 
-            ROS_INFO("X: %g", inFrontOfObjectEndEffectorPose.position.x);
-            ROS_INFO("Y: %g", inFrontOfObjectEndEffectorPose.position.y);
-            ROS_INFO("Z: %g", inFrontOfObjectEndEffectorPose.position.z);
-
             geometry_msgs::PoseStamped inFrontOfObjectEndEffectorPoseStamped;
             inFrontOfObjectEndEffectorPoseStamped.pose = inFrontOfObjectEndEffectorPose;
             inFrontOfObjectEndEffectorPoseStamped.header.frame_id = "map";
-            ROS_INFO_STREAM("POSESTAMPED FRAME: " << inFrontOfObjectEndEffectorPoseStamped.header.frame_id);
 
-            geometry_msgs::PointStamped newPoint;
             geometry_msgs::PointStamped oldPoint;
+            geometry_msgs::PointStamped newPoint;
             oldPoint.header.frame_id = inFrontOfObjectEndEffectorPoseStamped.header.frame_id;
             oldPoint.point = inFrontOfObjectEndEffectorPoseStamped.pose.position;
             listener.transformPoint("base_footprint", oldPoint, newPoint);
 
             //set new goal for movegroup and plan again with new goal
             group.setPoseTarget(inFrontOfObjectEndEffectorPoseStamped);
-            group.setGoalTolerance(0.015);
+            group.setGoalTolerance(0.025);
 
             error_code = group.plan(execution_plan);
 
             //if plan succeeded, move to goal
-            if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
+            if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS) {
                 error_code = group.move();
 
-        }
-
-        /*
-         * Then calculate pose to move endeffector to the object
-         */
-
-        //plan to move endeffector to object
-        geometry_msgs::PointStamped point;
-
-        ROS_INFO("Transforming Point from %s to %s", goal_point.header.frame_id.c_str(),
-                 group.getPlanningFrame().c_str());
-        listener.transformPoint(group.getPlanningFrame(), goal_point, point);
-
-        geometry_msgs::PoseStamped objectPosition;
-        objectPosition.header.frame_id = point.header.frame_id;
-        objectPosition.pose.position.x = point.point.x;
-        objectPosition.pose.position.y = point.point.y;
-        objectPosition.pose.position.z = point.point.z;
-        objectPosition.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI_2, 0, 0);
-
-        //set target for movegroup instance
-        group.setPoseTarget(objectPosition);
-        group.setGoalTolerance(0.015);
-
-        error_code = group.plan(execution_plan);
 
 
-        //if planning to move arm to the pose, recalculate last waypoint so that the endeffector is moved to
-        //object instead of the arm
-        if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS) {
+                /*
+                * If first movement was successful, calculate path to move endeffector to the object
+                */
 
-            robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-            robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+                //plan to move endeffector to object
+                geometry_msgs::PointStamped point;
 
-            robot_trajectory::RobotTrajectory trajectory(kinematic_model, group.getName());
-            trajectory.setRobotTrajectoryMsg(*(group.getCurrentState()), execution_plan.trajectory_);
+                ROS_INFO("Transforming Point from %s to %s", goal_point.header.frame_id.c_str(),
+                         group.getPlanningFrame().c_str());
+                listener.transformPoint(group.getPlanningFrame(), goal_point, point);
 
-            moveit::core::RobotState robotState = trajectory.getLastWayPoint();
-            Eigen::Affine3d eef_transform = robotState.getGlobalLinkTransform(group.getEndEffectorLink());
+                geometry_msgs::PoseStamped objectPosition;
+                objectPosition.header.frame_id = point.header.frame_id;
+                objectPosition.pose.position.x = point.point.x;
+                objectPosition.pose.position.y = point.point.y;
+                objectPosition.pose.position.z = point.point.z;
+                objectPosition.pose.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI_2, 0, 0);
 
-            geometry_msgs::Pose objectEndEffectorPose;
-            tf::poseEigenToMsg(eef_transform, objectEndEffectorPose);
+                //set target for movegroup instance
+                group.setPoseTarget(objectPosition);
+                group.setGoalTolerance(0.025);
 
-            if(group.getName() == "right_arm_group"){
-                objectEndEffectorPose.position.x -= GRIPPER_LENGTH_RIGHT/3;
-            }else{
-                objectEndEffectorPose.position.x -= GRIPPER_LENGTH_LEFT/3;
+                error_code = group.plan(execution_plan);
+
+
+                //if planning to move arm to the pose, recalculate last waypoint so that the endeffector is moved to
+                //object instead of the arm
+                if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS) {
+
+                    robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
+                    robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
+
+                    robot_trajectory::RobotTrajectory trajectory(kinematic_model, group.getName());
+                    trajectory.setRobotTrajectoryMsg(*(group.getCurrentState()), execution_plan.trajectory_);
+
+                    moveit::core::RobotState robotState = trajectory.getLastWayPoint();
+                    Eigen::Affine3d eef_transform = robotState.getGlobalLinkTransform(group.getEndEffectorLink());
+
+                    geometry_msgs::Pose objectEndEffectorPose;
+                    tf::poseEigenToMsg(eef_transform, objectEndEffectorPose);
+
+                    geometry_msgs::PoseStamped objectEndEffectorPoseStamped;
+                    objectEndEffectorPoseStamped.pose = objectEndEffectorPose;
+                    objectEndEffectorPoseStamped.header.frame_id = "map";
+
+                    geometry_msgs::PoseStamped objectEndEffectorPoseStampedNewHeight;
+                    listener.transformPose("base_footprint", objectEndEffectorPoseStamped, objectEndEffectorPoseStampedNewHeight);
+
+                    if(group.getName() == "right_arm_group"){
+                        objectEndEffectorPoseStampedNewHeight.pose.position.x += GRIPPER_LENGTH_RIGHT * 2/3;
+                    }else{
+                        objectEndEffectorPoseStampedNewHeight.pose.position.x += GRIPPER_LENGTH_LEFT * 2/3;
+                    }
+                    objectEndEffectorPoseStampedNewHeight.pose.position.z += 0.04;
+
+                    //set new goal for movegroup and plan again with new goal
+                    group.setPoseTarget(objectEndEffectorPoseStampedNewHeight);
+                    group.setGoalTolerance(0.015);
+
+                    geometry_msgs::PointStamped markerPoint;
+                    markerPoint.header = objectEndEffectorPoseStampedNewHeight.header;
+                    markerPoint.point = objectEndEffectorPoseStampedNewHeight.pose.position;
+                    publishVisualizationMarker(markerPoint, COLOR_SCHEMA_VISION);
+
+                    //von frontdirectionofobject trajectorie richtung markerpoint mit orientierung
+                    //setposereferenceframe auf base footprint
+                    //computecartesianpath
+
+                    geometry_msgs::Vector3 directionVector;
+                    directionVector.x = markerPoint.point.x - frontDirectionOfObject.point.x;
+                    directionVector.y = markerPoint.point.y - frontDirectionOfObject.point.y;
+                    directionVector.z = markerPoint.point.z - frontDirectionOfObject.point.z;
+
+                    visualization_msgs::Marker marker2;
+                    marker2.id = 5;
+                    marker2.header.stamp = ros::Time::now();
+                    marker2.header.frame_id = "/base_footprint";//point.header.frame_id;
+                    marker2.type = visualization_msgs::Marker::ARROW;
+                    marker2.action = visualization_msgs::Marker::ADD;
+                    marker2.points.push_back(frontDirectionOfObject.point);
+                    geometry_msgs::Point end;
+                    end.x = frontDirectionOfObject.point.x + directionVector.x;
+                    end.y = frontDirectionOfObject.point.y + directionVector.y;
+                    end.z = frontDirectionOfObject.point.z + directionVector.z;
+                    marker2.points.push_back(end);
+                    marker2.pose.orientation.x = 0.0;
+                    marker2.pose.orientation.y = 0.0;
+                    marker2.pose.orientation.z = 0.0;
+                    marker2.pose.orientation.w = 1.0;
+                    marker2.scale.x = 0.05;
+                    marker2.scale.y = 0.05;
+                    marker2.scale.z = 0.05;
+                    marker2.color.a = 0.7;
+                    marker2.ns = "waypoints";
+                    marker2.color.r = 0.2;
+                    marker2.color.g = 0.2;
+                    marker2.color.b = 0.5;
+                    //vis_pub.publish(marker2);
+
+                    /*ROS_INFO("STARTX:  %f", frontDirectionOfObject.point.x);
+                    ROS_INFO("STARTY:  %f", frontDirectionOfObject.point.y);
+                    ROS_INFO("STARTZ:  %f", frontDirectionOfObject.point.z);
+                    ROS_INFO("ENDX:  %f", end.x);
+                    ROS_INFO("ENDY:  %f", end.y);
+                    ROS_INFO("ENDZ:  %f", end.z);*/
+
+                    float distance = sqrt(pow(directionVector.x, 2.0) + pow(directionVector.y, 2.0)
+                            + pow(directionVector.z, 2.0));
+
+                    //sample 10 points for trajectory
+                    geometry_msgs::Vector3 directionStep;
+                    directionStep.x = directionVector.x / 10.0;
+                    directionStep.y = directionVector.y / 10.0;
+                    directionStep.z = directionVector.z / 10.0;
+
+                    std::vector<geometry_msgs::Pose> waypoints;
+
+
+                    visualization_msgs::Marker marker;
+                    marker.id = 0;
+                    marker.header.stamp = ros::Time::now();
+                    marker.header.frame_id = "/base_footprint";//point.header.frame_id;
+                    marker.type = visualization_msgs::Marker::SPHERE_LIST;
+                    marker.action = visualization_msgs::Marker::ADD;
+                    marker.pose.orientation.x = 0.0;
+                    marker.pose.orientation.y = 0.0;
+                    marker.pose.orientation.z = 0.0;
+                    marker.pose.orientation.w = 1.0;
+                    marker.scale.x = 0.03;
+                    marker.scale.y = 0.03;
+                    marker.scale.z = 0.03;
+                    marker.color.a = 0.7;
+                    marker.ns = "waypoints";
+                    marker.color.r = 0.5;
+                    marker.color.g = 0.5;
+                    marker.color.b = 0.0;
+                    for(double i = 1; i <= 10; i++){
+                        geometry_msgs::Pose waypoint;
+                        geometry_msgs::Vector3 step;
+
+                        step.x = directionVector.x * (i/10.0);
+                        step.y = directionVector.y * (i/10.0);
+                        step.z = directionVector.z * (i/10.0);
+
+                        waypoint.position.x = frontDirectionOfObject.point.x + step.x;
+                        waypoint.position.y = frontDirectionOfObject.point.y + step.y;
+                        waypoint.position.z = frontDirectionOfObject.point.z + step.z;
+                        waypoint.orientation = tf::createQuaternionMsgFromRollPitchYaw(M_PI_2, 0, 0);
+
+                        waypoints.push_back(waypoint);
+
+                        geometry_msgs::Point vis_marker;
+                        vis_marker.x = waypoint.position.x;
+                        vis_marker.y = waypoint.position.y;
+                        vis_marker.z = waypoint.position.z;
+                       // vis_marker.header.frame_id = "/base_footprint";
+                        //publishVisualizationMarker(vis_marker, COLOR_SCHEMA_MOTION);
+
+                        ROS_INFO(" X:  %f", waypoint.position.x);
+                        ROS_INFO(" Y:  %f", waypoint.position.y);
+                        ROS_INFO(" Z:  %f", waypoint.position.z);
+
+                        marker.points.push_back(vis_marker);
+                    }
+
+                    vis_pub.publish(marker);
+
+                    group.setPoseReferenceFrame("base_footprint");
+                    moveit_msgs::RobotTrajectory robotTrajectory;
+                    double fraction = group.computeCartesianPath(waypoints, 0.01, 0.0, robotTrajectory);
+
+                    if(fraction != -1){
+                        robot_trajectory::RobotTrajectory rt(group.getCurrentState()->getRobotModel(), group.getName());
+                        rt.setRobotTrajectoryMsg(*group.getCurrentState(), robotTrajectory);
+                        /*trajectory_processing::IterativeParabolicTimeParameterization iptp;
+                        success = iptp.computeTimeStamps(rt);
+                        ROS_INFO("Computed time stamp %s",success?"SUCCEDED":"FAILED");*/
+
+                        rt.getRobotTrajectoryMsg(robotTrajectory);
+
+
+
+                        execution_plan.trajectory_ = robotTrajectory;
+                        ROS_INFO("Visualizing plan 4 (cartesian path) (%.2f%% acheived)",fraction * 100.0);
+                        sleep(5.0);
+                        group.execute(execution_plan);
+
+
+
+                    }
+                    /*
+                    error_code = group.plan(execution_plan);
+
+                    //if plan succeeded, move to goal
+                    if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
+                        error_code = group.move();*/
+
+                }
             }
 
-            geometry_msgs::PoseStamped objectEndEffectorPoseStamped;
-            objectEndEffectorPoseStamped.pose = objectEndEffectorPose;
-            objectEndEffectorPoseStamped.header.frame_id = "map";
-
-            geometry_msgs::PoseStamped objectEndEffectorPoseStampedNewHeight;
-            listener.transformPose("base_footprint", objectEndEffectorPoseStamped, objectEndEffectorPoseStampedNewHeight);
-
-            objectEndEffectorPoseStampedNewHeight.pose.position.z += 0.03;
-
-            //set new goal for movegroup and plan again with new goal
-            group.setPoseTarget(objectEndEffectorPoseStampedNewHeight);
-            group.setGoalTolerance(0.015);
-
-            geometry_msgs::PointStamped markerPoint;
-            markerPoint.header = objectEndEffectorPoseStampedNewHeight.header;
-            markerPoint.point = objectEndEffectorPoseStampedNewHeight.pose.position;
-            publishVisualizationMarker(markerPoint, COLOR_SCHEMA_VISION);
-
-            error_code = group.plan(execution_plan);
-
-            //if plan succeeded, move to goal
-            if (error_code.val == moveit_msgs::MoveItErrorCodes::SUCCESS)
-                error_code = group.move();
-
         }
+
+
     }
 };
 
@@ -573,7 +697,7 @@ public:
         ros::init(argc, argv, "motion_main");
         ros::NodeHandle node_handle;
 
-        ros::Publisher pub = node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 0);
+        ros::Publisher pub = node_handle.advertise<visualization_msgs::Marker>("visualization_marker", 10);
         Main main(node_handle, pub);
 
         //add kitchen models to collision detection matrix
