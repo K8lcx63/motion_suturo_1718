fb265f8ac7e87618f24fd365f9c647416e05a77f
Unknown <mbertram@uni-bremen.de>
2018-04-20 13:25:05 +0200

    remove trial test

--- motion_gripper/test/gripper_test.cpp
+++ motion_gripper/test/gripper_test.cpp
@@ -1,11 +1,7 @@
 #include <gtest/gtest.h>
 #include <Gripper.h>
 
-TEST(MaxTest, gripper_test) {
-    ASSERT_TRUE(true);
-}
-
-TEST(GripperTest, test_message_generation) {
+TEST(GripperTest, message_generation) {
     pr2_controllers_msgs::Pr2GripperCommandGoal result = Gripper::generateGripperGoal(1.1, 5.5);
     ASSERT_EQ(result.command.position, 1.1f);
     ASSERT_EQ(result.command.max_effort, 5.5f);
