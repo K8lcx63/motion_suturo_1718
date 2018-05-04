d5cf2d599ef23498d81164e4bb14398fa0c6e800
Unknown <mbertram@uni-bremen.de>
2018-05-04 13:06:31 +0200

    tests for vis marker

--- motion/src/visualization/visualization_marker.cpp
+++ motion/src/visualization/visualization_marker.cpp
@@ -22,33 +22,39 @@ void VisualizationMarker::publishMeshes(const geometry_msgs::PoseArray &poses, s
 
     for(int i = 0; i < poses.poses.size(); i++){
 
-        visualization_msgs::Marker marker;
-
-        marker.header.frame_id = poses.header.frame_id;
-        marker.header.stamp = ros::Time(0);
-        marker.ns = "gripper_meshes";
-        marker.id = i;
-        marker.type = visualization_msgs::Marker::MESH_RESOURCE;
-        marker.action = visualization_msgs::Marker::ADD;
-
-        marker.pose = poses.poses[i];
-
-        marker.scale.x = 0.8;
-        marker.scale.y = 0.8;
-        marker.scale.z = 0.8;
-
-        marker.color.a = 1.0;
-        marker.color.r = 0.5;
-        marker.color.g = 0.5;
-        marker.color.b = 0.5;
-
-        marker.mesh_resource = path;
+        visualization_msgs::Marker marker = generatePoseMarker(poses.poses[i], path, i, poses.header.frame_id);
 
         visualizedPoses.markers.push_back(marker);
     }
 
 
     visualizationMarkerPub.publish( visualizedPoses );
+}
+
+visualization_msgs::Marker
+VisualizationMarker::generatePoseMarker(const geometry_msgs::Pose &pose, const std::string &path, int id, std::string frame) {
+    visualization_msgs::Marker marker;
+
+    marker.header.frame_id = frame;
+    marker.header.stamp = ros::Time(0);
+    marker.ns = "gripper_meshes";
+    marker.id = id;
+    marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+    marker.action = visualization_msgs::Marker::ADD;
+
+    marker.pose = pose;
+
+    marker.scale.x = 0.8;
+    marker.scale.y = 0.8;
+    marker.scale.z = 0.8;
+
+    marker.color.a = 1.0;
+    marker.color.r = 0.5;
+    marker.color.g = 0.5;
+    marker.color.b = 0.5;
+
+    marker.mesh_resource = path;
+    return marker;
 }
 
 void VisualizationMarker::publishMeshesWithColor(const std::vector<geometry_msgs::Pose> &poses, const std::string frameId, const std::vector<int> ids, std::string path,
