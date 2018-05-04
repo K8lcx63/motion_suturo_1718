9ea47cddd9be46e086bb70fcb32a08e3c8fd46f9
Unknown <rhaak@uni-bremen.de>
2018-05-04 16:00:46 +0200

    grenzwert für force beim abstellen angepasst

--- motion/include/group_controller.h
+++ motion/include/group_controller.h
@@ -32,7 +32,7 @@ private:
     const float TABLE_HEIGHT = 0.84f;
     const float MAXIMUM_OBJECT_HEIGHT = 0.30f;
     const float LIFTING_AFTER_GRASPING = 0.08f;
-    const float FORCE_THRESHOLD = 15;
+    const float FORCE_THRESHOLD = 11;
     const int MAX_ATTEMPTS_TO_GET_IK_SOLUTION = 2;
 
     const string PATH_TO_GRIPPER_MESH = "package://knowledge_common/meshes/Gripper/Gripper.stl";
