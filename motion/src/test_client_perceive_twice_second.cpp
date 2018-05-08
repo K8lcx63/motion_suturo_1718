b53dc2d36ea7d8092f64fac15ee19ebea14ad587
Kevin <stoermer@uni-bremen.de>
2018-05-08 11:23:26 +0200

    test client

--- motion/src/test_client_perceive_twice_second.cpp
+++ motion/src/test_client_perceive_twice_second.cpp
@@ -23,9 +23,9 @@ int main(int argc, char **argv)
   geometry_msgs::PoseStamped object_pose;
   object_pose.header.stamp = ros::Time::now();
   object_pose.header.frame_id = "/map";
-  object_pose.pose.position.x = -1.508618;
-  object_pose.pose.position.y = -0.500126;
-  object_pose.pose.position.z = 0.902383;
+  object_pose.pose.position.x = -0.928618;
+  object_pose.pose.position.y = 0.590126;
+  object_pose.pose.position.z = 0.932383;
   object_pose.pose.orientation.x = 0.0;
   object_pose.pose.orientation.y = 0.0;
   object_pose.pose.orientation.z = 0.0;
